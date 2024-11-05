65771f6df6f0c96ca7cb9a1ac260f26a2e11f8ef
Weedanta <baguswedanta17@gmail.com>
2024-11-06 00:32:29 +0700

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GreetController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello/{name}', [GreetController::class, 'greet']);
+
+
+
